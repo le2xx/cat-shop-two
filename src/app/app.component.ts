b2653e4db14fb938c2f44baa6f977b7ab146b461
le2xx <le2x@rambler.ru>
2019-01-30 16:08:14 +0300

    Added fonts, images, variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.pug',
   styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
   title = 'cat-shop-two';
 }
+
